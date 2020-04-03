777cda8c8e75e11eafff9d835f4a57d132fbb776
Werbth Rocha <werbth@gmail.com>
2020-04-02 23:04:01 -0300

    Override rest template for connection config connector

--- src/main/java/org/entando/plugins/pda/EntandoPdaPluginJavaApplication.java
+++ src/main/java/org/entando/plugins/pda/EntandoPdaPluginJavaApplication.java
@@ -1,16 +1,22 @@
 package org.entando.plugins.pda;
 
+import static org.entando.connectionconfigconnector.config.ConnectionConfigConfiguration.CONFIG_REST_TEMPLATE;
+
 import org.entando.connectionconfigconnector.config.ConnectionConfigConfiguration;
 import org.entando.connectionconfigconnector.service.ConnectionConfigConnector;
 import org.entando.connectionconfigconnector.service.impl.ConnectionConfigConnectorImpl;
 import org.entando.connectionconfigconnector.service.impl.InMemoryConnectionConfigConnector;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -51,5 +57,14 @@ public class EntandoPdaPluginJavaApplication {
                         .allowCredentials(true);
             }
         };
+    }
+
+    @Bean
+    @Qualifier(CONFIG_REST_TEMPLATE)
+    @Primary
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        RootUriTemplateHandler.addTo(restTemplate, "http://localhost:8084");
+        return restTemplate;
     }
 }
